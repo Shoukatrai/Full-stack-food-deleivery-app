e143a00642db205dd2e45e109ad3f2468fedb968
shoukatrai <raishoukat033@gmail.com>
2025-07-19 22:34:29 +0500

    network issues

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(express.json()) //body parser
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cors({
-  origin: ['http://localhost:5173' , 'https://food-delivery-app-sr.netlify.app'],
+  origin: 'https://food-delivery-app-sr.netlify.app',
   credentials: true
 }));
 
